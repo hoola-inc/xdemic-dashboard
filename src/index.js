2401e5fdd4a8d5612135b0036033139d6cf9b213
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-01-02 15:08:58 +0500

    integration redux, adding constants, reducer, state, main store of the app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
 
+import configureStore from "./configureStore";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create redux store with history
+const initialState = {};
+const store = configureStore(
+  initialState
+  //history
+);
+
+const MOUNT_NODE = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  MOUNT_NODE
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
