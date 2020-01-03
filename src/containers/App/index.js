2705d86dfc2913a18606b6e0f7b9612d3925d071
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-01-03 16:00:44 +0500

    adding app container

---
+++ src/containers/App/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Layout } from "antd";
+
+class AppContainer extends Component {
+  render() {
+    return <div>App Container</div>;
+  }
+}
+
+export default AppContainer;
