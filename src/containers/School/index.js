7ddaf283ba8d87f1ec7523d849bde9bb6706176c
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-01-03 16:01:39 +0500

    adding school container

---
+++ src/containers/School/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SchoolContainer extends Component {
+  render() {
+    return <div>School Container</div>;
+  }
+}
+
+export default SchoolContainer;
