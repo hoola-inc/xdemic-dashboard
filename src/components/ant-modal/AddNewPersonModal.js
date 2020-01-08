23600c3ff3818f81756cc8d11dff7a57564d1395
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-01-08 15:57:59 +0500

    generic the add new person modal for person & student

--- src/components/ant-modal/AddNewPersonModal.js
+++ src/components/ant-modal/AddNewPersonModal.js
@@ -1,131 +1,133 @@
-import React from 'react';
-import axios from 'axios';
-import { Modal, Button, Input, Form, DatePicker, message } from 'antd';
+import React from "react";
+import axios from "axios";
+import { Modal, Button, Input, Form, DatePicker, message } from "antd";
 
 class AddNewPersonModal extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            visible: false,
-            loading: false,
-            iconLoading: false,
-            email: '',
-        };
-
-    }
-
-    showModal = () => {
-        this.setState({
-            visible: true,
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+      loading: false,
+      iconLoading: false,
+      email: ""
     };
+  }
 
-    handleOk = e => {
-        this.setState({
-            visible: false,
-        });
-    };
+  showModal = () => {
+    this.setState({
+      visible: true
+    });
+  };
 
-    handleCancel = e => {
-        this.setState({
-            visible: false,
-        });
-    };
+  handleOk = e => {
+    this.setState({
+      visible: false
+    });
+  };
 
-    enterLoading = () => {
-        this.setState({ loading: true });
-    };
+  handleCancel = e => {
+    this.setState({
+      visible: false
+    });
+  };
 
-    enterIconLoading = () => {
-        this.setState({ iconLoading: true });
-    };
+  enterLoading = () => {
+    this.setState({ loading: true });
+  };
 
-    changeHandler = e => {
-        this.setState({ [e.target.name]: e.target.value });
-    }
+  enterIconLoading = () => {
+    this.setState({ iconLoading: true });
+  };
 
-    submitHandler = e => {
-        e.preventDefault();
-        this.enterLoading();
-        // this.handleSubmit()
-        this.sendCourse();
-    }
+  changeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    sendCourse = () => {
-        axios.post('https://xdemic-api.herokuapp.com/email', this.state)
-            .then(res => {
-                if (res.data.status) {
-                    this.setState({ loading: false });
-                    message.success('Email Sent');
-                    this.handleCancel();
-                }
-                else {
-                    message.error('Something went wrong!');
-                    this.handleCancel();
-                    this.setState({ loading: false })
-                }
-            })
-            .catch(err => {
-                console.log('An Error occured while sending Email ::: ', err.message);
-                message.error(err.message);
-                this.setState({ loading: false });
-                this.handle.onCancel();
-            });
-    }
+  submitHandler = e => {
+    e.preventDefault();
+    this.enterLoading();
+    // this.handleSubmit()
+    this.sendCourse();
+  };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-            }
-        });
-    };
+  sendCourse = () => {
+    axios
+      .post("https://xdemic-api.herokuapp.com/email", this.state)
+      .then(res => {
+        if (res.data.status) {
+          this.setState({ loading: false });
+          message.success("Email Sent");
+          this.handleCancel();
+        } else {
+          message.error("Something went wrong!");
+          this.handleCancel();
+          this.setState({ loading: false });
+        }
+      })
+      .catch(err => {
+        console.log("An Error occured while sending Email ::: ", err.message);
+        message.error(err.message);
+        this.setState({ loading: false });
+        this.handle.onCancel();
+      });
+  };
 
-    render() {
-        const { email } = this.state;
-        const { getFieldDecorator } = this.props.form;
-        return (
-            <div>
-                <Button block size="default" onClick={this.showModal}>
-                    + Add Person
-                </Button>
-                <Modal
-                    title="Enter Person Email"
-                    visible={this.state.visible}
-                    onOk={this.handleOk}
-                    onCancel={this.handleCancel}
-                >
-                    <Form onSubmit={this.submitHandler}>
-                        <Form.Item label="Person Email">
-                            {getFieldDecorator('email', {
-                                rules: [{ required: true, message: 'Please input person email!' }],
-                            })(
-                                <Input
-                                    placeholder="enter person email"
-                                    allowClear
-                                    name="email"
-                                    value={email}
-                                    onChange={this.changeHandler}
-                                />
-                            )}
-                        </Form.Item>
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
 
-                        <Button type="primary" loading={this.state.loading} onClick={this.submitHandler} ghost >
-                            Submit
-                        </Button>
+  render() {
+    const { email } = this.state;
+    const { getFieldDecorator } = this.props.form;
+    const { name } = this.props;
+    return (
+      <div>
+        <Button block size="default" onClick={this.showModal}>
+          + Add {name}
+        </Button>
+        <Modal
+          title={`Enter ${name} Email`}
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <Form onSubmit={this.submitHandler}>
+            <Form.Item label={`${name} Email`}>
+              {getFieldDecorator("email", {
+                rules: [
+                  { required: true, message: `Please input ${name} email!` }
+                ]
+              })(
+                <Input
+                  placeholder={`Enter ${name} email`}
+                  allowClear
+                  name="email"
+                  value={email}
+                  onChange={this.changeHandler}
+                />
+              )}
+            </Form.Item>
 
-                    </Form>
-                </Modal>
-            </div>
-
-        );
-    }
+            <Button
+              type="primary"
+              loading={this.state.loading}
+              onClick={this.submitHandler}
+              ghost
+            >
+              Submit
+            </Button>
+          </Form>
+        </Modal>
+      </div>
+    );
+  }
 }
-
-
 
 const AddNewPerson = Form.create()(AddNewPersonModal);
 
-export default AddNewPerson; 
+export default AddNewPerson;
