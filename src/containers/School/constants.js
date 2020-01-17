43ae2321fe1e51ad0718d0e413a4e3adb278fac5
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-01-17 14:43:00 +0500

    adding different school action types

--- src/containers/School/constants.js
+++ src/containers/School/constants.js
@@ -10,3 +10,7 @@
  */
 
 export const ADD_SCHOOL = "xDemicDashboard/School/ADD_SCHOOL";
+export const DELETE_SCHOOL = "xDemicDashboard/School/DELETE_SCHOOL";
+export const EDIT_SCHOOL = "xDemicDashboard/School/EDIT_SCHOOL";
+export const ACCEPT_SCHOOL = "xDemicDashboard/School/ACCEPT_SCHOOL";
+export const REJECT_SCHOOL = "xDemicDashboard/School/REJECT_SCHOOL";
