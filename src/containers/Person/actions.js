be66ed5960069357f69f07370cdbbf049cedba7a
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-01-20 17:03:13 +0500

    adding new person action creator

--- src/containers/Person/actions.js
+++ src/containers/Person/actions.js
@@ -21,29 +21,36 @@ import HS from "../../services/HttpService";
 /**
  *
  * @param  {object} data The data
+ * @return  {object} ADD_ADMIN An action object with a type of ADD_ADMIN passing the data
  *
- * @return {object}       An action object with a type of ADD_ADMIN passing the data
  */
 
-export function addSchool(data) {
+export function addPerson(data) {
   return {
     type: ADD_PERSON,
     data
   };
 }
 
-export function addNewSchool(data) {
-  console.log("addNewSchool is calling with data is: ", data);
+/**
+ *
+ * @param  {object} data The data of new person
+ * @return  {object} An action creator
+ *
+ */
+
+export function addNewPerson(data) {
+  console.log("addNewPerson is calling with data is: ", data);
   return function(dispatch) {
-    return HS.post("school", data).then(res => {
-      console.log("add new school response is: ", res);
-      dispatch(addSchool(data));
+    return HS.post("persons", data).then(res => {
+      console.log("add new person response is: ", res);
+      dispatch(addPerson(data));
     });
   };
 }
 
-export function fetchSchool(schoolDid) {
-  console.log("fetchPosts schoolDid is: ", schoolDid);
+export function fetchPerson(personDid) {
+  console.log("fetchPerson person Did is: ", personDid);
   // Thunk middleware knows how to handle functions.
   // It passes the dispatch method as an argument to the function,
   // thus making it able to dispatch actions itself.
@@ -51,13 +58,13 @@ export function fetchSchool(schoolDid) {
     // First dispatch: the app state is updated to inform
     // that the API call is starting.
 
-    // dispatch(requestPosts(schoolDid));
+    // dispatch(requestPosts(personDid));
 
     // The function called by the thunk middleware can return a value,
     // that is passed on as the return value of the dispatch method.
     // In this case, we return a promise to wait for.
     // This is not required by thunk middleware, but it is convenient for us.
-    return fetch(`https://www.reddit.com/r/${schoolDid}.json`)
+    return fetch(`https://www.reddit.com/r/${personDid}.json`)
       .then(
         response => response.json(),
 
@@ -71,7 +78,7 @@ export function fetchSchool(schoolDid) {
         console.log("calling fetchPosts then 2");
         // We can dispatch many times!
         // Here, we update the app state with the results of the API call.
-        // dispatch(receivePosts(schoolDid, json))
+        // dispatch(receivePosts(personDid, json))
       });
   };
 }
