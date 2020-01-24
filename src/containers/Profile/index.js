6f6e6bfa34afee409a17cbe3bd883195cd8684d1
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-01-24 15:19:54 +0500

    also added in profile

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { Layout, Row, Col } from 'antd';
-import Sidebar from '../../components/common/Sidebar';
-import Header from '../../components/common/Header';
+import { Layout, Row, Col } from "antd";
+import Sidebar from "../../components/common/Sidebar";
+import Header from "../../components/common/Header";
 
 class Profile extends Component {
   render() {
     return (
-      <Layout style={{ minHeight: '100vh' }}>
+      <Layout style={{ minHeight: "100vh" }}>
         <Sidebar />
         <Layout>
           <Header />
           <Row>
             <Col span={22} offset={1}>
-              <h1> Hi! Rizwan </h1>
+              <h1 style={{ textTransform: "capitalize" }}>
+                Hi, {this.props.name}
+              </h1>
             </Col>
           </Row>
         </Layout>
@@ -24,8 +26,9 @@ class Profile extends Component {
 }
 
 const mapStateToProps = state => {
+  const localUserData = JSON.parse(localStorage.getItem("userData"));
   return {
-    userData: state.admin.userData || [{ name: "Rizwan" }]
+    name: state.admin.userData.fullName || localUserData.fullName
   };
 };
 
