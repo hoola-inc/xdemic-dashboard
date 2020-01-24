81c5c5d0a8e5301520c4ac30e42b9b75035afc8b
HamzaBinSarfraz <muhammadameerhmza@gmail.com>
2020-01-24 15:13:09 +0500

    common components added in profile

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import { Layout, Row, Col } from 'antd';
+import Sidebar from '../../components/common/Sidebar';
+import Header from '../../components/common/Header';
 
 class Profile extends Component {
   render() {
-    return <div>Profile</div>;
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sidebar />
+        <Layout>
+          <Header />
+          <Row>
+            <Col span={22} offset={1}>
+              <h1> Hi! Rizwan </h1>
+            </Col>
+          </Row>
+        </Layout>
+      </Layout>
+    );
   }
 }
 
